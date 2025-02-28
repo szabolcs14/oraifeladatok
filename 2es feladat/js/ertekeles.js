c308fb3046e02cec25074cc68bdded31963a253c
scheda balint <scheda.balint.jozsef.2022i@bankitatabanya.hu>
2025-02-28 11:27:58 +0100

    kesz

--- 2es feladat/js/ertekeles.js
+++ 2es feladat/js/ertekeles.js
@@ -1,3 +1,39 @@
+var emailLista = [];
+
 function kuldes() {
-    alert("Sikeres bevitel!");
+
+    alert("Sikeres értékelés!");
+    var ertekelendo = document.querySelector(".ertekeles").value;
+    for (let i = 0; i < ertekelendo.length; i++) {
+        document.write("*");
+    }
 }
+function ertekelVizsgal(ertekelendo) {
+    var ertekelendo = document.querySelector(".ertekeles").value;
+
+    for (let i = 0; i < ertekelendo.length; i++) { 
+    if(ertekelendo.length == 0){
+        alert("Nincs értékleve!");
+        return false;
+    } else if (ertekelendo.length == 1){
+        ertekelendo[0].replace(i, "*");
+        return false;
+    } else if (ertekelendo.length == 2) {
+        ertekelendo.replaceAll(i,"*");
+        return false;
+    } else if (ertekelendo.length == 3){
+        ertekelendo.replaceAll(i,"*");
+        return false;
+    } else if (ertekelendo.length == 4){
+        ertekelendo.replaceAll(i,"*");
+        return false;
+    }else if (ertekelendo.length == 5){
+        ertekelendo.replaceAll(i,"*");
+        return false;
+    }
+}
+alert("Sikeres értékelés!");
+return true;
+}
+
+///<i class="bi bi-star-fill"></i>
