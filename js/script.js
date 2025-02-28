ed19451e54f8c27c9fb7b035daa8713ba1fcc7c5
scheda balint <scheda.balint.jozsef.2022i@bankitatabanya.hu>
2025-02-28 08:55:03 +0100

    Elso feladat
    megosztás

---
+++ js/script.js
@@ -0,0 +1,20 @@
+function kuldes(){
+    var jelszo = document.querySelector(".jelszo").value;
+
+    const szamok = "0123456789";
+    const karakterek = "#&*!%";
+    const nagyBetuk = "AÁBCDEÉFGHIÍJKLMNOÓÖŐPQRSTUÚÜŰVWXYZ";
+
+    while (jelszo == ""){
+            alert("Nincs megadva jelszó! a jelszó gyenge"); 
+        } else {
+           if (jelszo.length < 8) {
+            alert("A jelszó túl rövid ! A jelszó kevésbé-gyenge");
+        } else if (jelszo.includes(szamok[i])) {
+            alert("A jelszó erőssége közepes");
+        } else if (jelszo.includes(karakterek[i])) {
+            alert("A jelszó erőssége normál");
+        }
+    }
+
+}
